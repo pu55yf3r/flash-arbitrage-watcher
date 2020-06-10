77f7b0fae98d31415a570318320dd7ff04256625
pu55yf3r <pu55yf3r@gmail.com>
2020-06-10 11:34:50 +0300

    Updates

---
+++ precache-manifest.5c227a077bc134078c2bcdd312da8f3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c9c27cdb0e0f15e447621f94f63468",
+    "url": "/flash-arbitrage-watcher/index.html"
+  },
+  {
+    "revision": "d573d4c44e9d033a9266",
+    "url": "/flash-arbitrage-watcher/static/css/main.575b549f.chunk.css"
+  },
+  {
+    "revision": "d9d8beb62be1cda97894",
+    "url": "/flash-arbitrage-watcher/static/js/2.7c21d972.chunk.js"
+  },
+  {
+    "revision": "b0235c4a03082a633ba67a107e0c1969",
+    "url": "/flash-arbitrage-watcher/static/js/2.7c21d972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d573d4c44e9d033a9266",
+    "url": "/flash-arbitrage-watcher/static/js/main.45fa83f1.chunk.js"
+  },
+  {
+    "revision": "d244a11a403a8dc4052f",
+    "url": "/flash-arbitrage-watcher/static/js/runtime-main.04536111.js"
+  }
+]);
